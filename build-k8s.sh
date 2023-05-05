72416f17058b87544ec4272bf5f512d9a2f9f5c2
lukas-pastva <info@lukaspastva.sk>
2023-05-05 07:20:55 +0200

    feat: added kaniko

--- build-k8s.sh
+++ build-k8s.sh
@@ -2,18 +2,18 @@
 
 echo "Building image on Kubernetes via Kaniko, not using Docker build. Not using Docker, using Containerd"
 
-export DESTINATION=${CI_REGISTRY_IMAGE}/${CI_PROJECT_NAME}:${CI_COMMIT_SHORT_SHA}
-export DESTINATION_LATEST=${CI_REGISTRY_IMAGE}/${CI_PROJECT_NAME}:latest
-export CONTEXT=$(echo $CI_REPOSITORY_URL | sed 's#https://#git://#g')
+export DESTINATION=${DESTINATION:-${CI_REGISTRY_IMAGE}/${CI_PROJECT_NAME}:${CI_COMMIT_SHORT_SHA}}
+export DESTINATION_LATEST=${DESTINATION_LATEST:-${CI_REGISTRY_IMAGE}/${CI_PROJECT_NAME}:latest}
+export CONTEXT=$(echo "$CI_REPOSITORY_URL#refs/heads/${CI_COMMIT_BRANCH}#${CI_COMMIT_SHA}" | sed 's|https://|git://|g')
 export HASH=$(openssl rand -hex 4 | tr '[:upper:]' '[:lower:]')
 export DOCKERCONFIGJSON=$(echo "{\"auths\":{\"$CI_REGISTRY\":{\"auth\":\"$(echo -n ${CI_REGISTRY_USER}:${CI_REGISTRY_PASSWORD} | base64)\"}}}" | base64 -w 0)
 
 envsubst < /usr/local/bin/kaniko.yml > kaniko-${HASH}.yml
 
 kubectl apply -f kaniko-${HASH}.yml
-sleep 15
-
-kubectl -n sys-runner logs -f kaniko-${HASH}
 sleep 5
 
-# kubectl delete -f kaniko-${HASH}.yml
+kubectl -n sys-runner-dind logs -f kaniko-${HASH}
+sleep 5
+
+kubectl delete -f kaniko-${HASH}.yml
