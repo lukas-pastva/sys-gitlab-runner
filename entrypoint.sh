83b43bd23a4cfadd6a276af03ff43be9edbd8a7f
Lukas Pastva <lukas.pastva>
2024-03-01 11:57:25 +0100

    feat: Refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "------------------------------------------------------------------------"
 echo "Installing ..."
 docker-php-ext-configure imap --with-kerberos --with-imap-ssl
 docker-php-ext-install imap
@@ -8,4 +9,6 @@ if [ -n "${DOCKER_GID}" ]; then
     groupmod -g "${DOCKER_GID}" docker
 fi
 
+echo "------------------------------------------------------------------------"
+echo "Starting runner ..."
 gitlab-runner run
